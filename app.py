4923d54f982bed79fa7a6e8b11e2e2d6a2405a6c
iamdrugs000 <123847467+iamdrugs000@users.noreply.github.com>
2023-02-03 11:36:25 +0900

    Add files via upload
    Web_develop/mainpage.html 기반으로 기록하기 누르면 기록되었습니다!라는 문구 뜨고 DB에 저장되는 것까지 작성했습니다.

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask , render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://iamdrugs000:sparta@cluster0.kcroqej.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+# @app.route('/foods', methods=['GET'])
+# def foods_get():
+# title_receive = request.args.get('title_give')
+# print(title_receive)
+# return jsonify({'result':'success', 'msg': '기록되었습니다!'})
+
+@app.route('/foods', methods=['POST'])
+def foods_post():
+    username_receive = request.form['username_give']
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+    image_receive = request.form['image_give']
+
+    doc = {
+         'username': username_receive,
+         'url': url_receive,
+         'star': star_receive,
+         'comment': comment_receive,
+         'image': image_receive
+   }
+    db.foods.insert_one(doc)
+    return jsonify({'msg': '기록되었습니다!'})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
