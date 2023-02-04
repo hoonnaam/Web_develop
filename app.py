af42b7c2bdad53bc1b1dbf48a2c81de8c4694699
jeonyj216 <jeonyj216@gmail.com>
2023-02-04 19:21:59 +0900

    add landing page.js

--- app.py
+++ app.py
@@ -18,37 +18,6 @@ def foods_get():
     foods_list = list(db.foods.find({}, {'_id': False}))
     return jsonify({'foods':foods_list})
 
-## 닉네임 저장
-@app.route("/api/nickname", methods=["POST"])
-def name_init():
-    # user 의 닉네임을 받아옵니다.
-    name_receive = request.form['name_give']
-
-    # user의 닉네임이 중복인지 확인합니다.
-    users_list = list(db.users.find({}, {'_id': False}))
-    for user in users_list:
-        if name_receive == user.get('name'):
-            return jsonify({'msg': '중복입니다!'})
-
-    ## Result 값 0으로 init
-    result_receive = 0
-
-    ## url 값 임의의 url 값으로 init
-    url_receive = ''
-
-    ## result_list 값 0으로 init
-    result_list_receive = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-    doc = {
-        'name': name_receive,
-        'result': result_receive,
-        'image': url_receive,
-        'result_list': result_list_receive
-    }
-
-    db.users.insert_one(doc)
-
-    return jsonify({'msg': '동네이름과 닉네임 등록 완료! \n구경하러 가기!'})
 
 @app.route('/foods', methods=['POST'])
 def foods_post():
